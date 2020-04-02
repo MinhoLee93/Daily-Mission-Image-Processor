2c7f671afc3752b53b211e4f95bf02031c275d18
Minho Lee <alsgkgk77@gmail.com>
2020-04-02 09:52:10 +0900

    Provider github 추가

--- src/main/java/com/dailymission/api/springboot/web/repository/user/AuthProvider.java
+++ src/main/java/com/dailymission/api/springboot/web/repository/user/AuthProvider.java
@@ -5,9 +5,10 @@ package com.dailymission.api.springboot.web.repository.user;
 * The User class contains information about the authentication provider.
 * Following is the definition of the AuthProvider enum -
 * */
-public enum AuthProvider {
+public enum  AuthProvider {
     local,
     google,
     naver,
-    kakao
+    kakao,
+    github
 }
